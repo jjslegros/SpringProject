5a7b660d6d806a78cc102e7289b6466bd0dc8e23
rlegros.cdi <rlegros.cdi@PC8-ECOLE.FORMATION.LOCAL>
2018-11-16 13:12:52 +0400

    login richard

---
+++ springproject/src/test/java/login/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package login.service;
+
+import login.model.User;
+import login.repository.RoleRepository;
+import login.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class UserServiceTest {
+
+    
+}

4b5f1b0ea328063f58c69fa784e21e1e75e9fcfe
rlegros.cdi <rlegros.cdi@PC8-ECOLE.FORMATION.LOCAL>
2018-11-16 09:35:03 +0400

    push de richard

--- springproject/src/main/java/com/example/springproject/utilisateur.java
+++ springproject/src/main/java/com/example/springproject/utilisateur.java
@@ -26,4 +26,30 @@ public class utilisateur {
                 "utilisateur[id=%d, firstName='%s', lastName='%s']",
                 id, firstName, lastName);
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+    
+    
 }
